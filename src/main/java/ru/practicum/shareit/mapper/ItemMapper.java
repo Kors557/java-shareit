b8176fe055eba5e4e8fd329b21100e416ced7c4f
Kors557 <1500459@mail.ru>
2024-09-30 08:44:26 +0300

    First

--- src/main/java/ru/practicum/shareit/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/mapper/ItemMapper.java
@@ -1,30 +1,16 @@
 package ru.practicum.shareit.mapper;
 
+import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemWithBookingsDto;
 import ru.practicum.shareit.item.model.Item;
 
-public class ItemMapper {
-    public static ItemDto toItemDto(Item item) {
-        if (item == null) {
-            return null;
-        }
-        return ItemDto.builder()
-                .id(item.getId())
-                .name(item.getName())
-                .description(item.getDescription())
-                .available(item.getAvailable())
-                .build();
-    }
+@Mapper
+public interface ItemMapper {
 
-    public static Item toItem(ItemDto itemDto) {
-        if (itemDto == null) {
-            return null;
-        }
-        return Item.builder()
-                .id(itemDto.getId())
-                .name(itemDto.getName())
-                .description(itemDto.getDescription())
-                .available(itemDto.getAvailable())
-                .build();
-    }
+    ItemDto mapToItemDto(Item item);
+
+    Item mapToItem(ItemDto itemDto);
+
+    ItemWithBookingsDto mapToItemWithBookingsDto(Item item);
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.user;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.mapper.UserMapper;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
@@ -11,22 +12,22 @@ import ru.practicum.shareit.user.service.UserService;
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userServiceImpl;
+    private final UserMapper userMapper;
 
     @GetMapping("/{userId}")
     public UserDto getUser(@PathVariable long userId) {
-        return userServiceImpl.getUser(userId);
+        return userMapper.toUserDto(userServiceImpl.getUser(userId));
     }
 
     @PostMapping
     public UserDto createUser(@Valid @RequestBody UserDto userDto) {
-        return userServiceImpl.createUser(userDto);
+        return userMapper.toUserDto(userServiceImpl.createUser(userDto));
     }
 
     @PatchMapping("/{userId}")
     public UserDto updateUser(@PathVariable long userId, @RequestBody UserDto userDto) {
-        return userServiceImpl.updateUser(userId, userDto);
+        return userMapper.toUserDto(userServiceImpl.updateUser(userId, userDto));
     }
 
     @DeleteMapping("/{userId}")
