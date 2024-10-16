48a2525ed1eab2448013bbec398c48f2c1a536ff
Kors557 <1500459@mail.ru>
2024-10-16 22:25:38 +0300

    tests 999

--- gateway/src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
+++ gateway/src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
@@ -1,11 +1,10 @@
 package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
 import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Set;
 
